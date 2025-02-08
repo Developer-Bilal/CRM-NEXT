d5c8cfb2a74e94013f3c7d90dd9a1d6dd2879c2f
Muhammad Bilal <bilalchanna67@gmail.com>
2025-02-08 11:47:42 +0500

    authentication error fixed

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,48 +1,95 @@
 import type { NextAuthOptions } from "next-auth";
 import Github from "next-auth/providers/github";
 import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
+interface User {
+  _id: string;
+  name: string;
+  email: string;
+  isAdmin: string;
+  phone: string;
+  country: string;
+  profilePhoto: string;
+  linkedin: string;
+  additionalInfo: string;
+  password: string;
+}
 
 export const options: NextAuthOptions = {
-    providers: [
-        Github({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string
-        }),
-        Credentials({
-            name: "credentials",
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    Credentials({
+      name: "credentials",
 
-            credentials: {
-                username: {
-                    label: "Username",
-                    type: "text",
-                    placeholder: "Your Name"
-                },
-                password: {
-                    label: "Password",
-                    type: "text",
-                    placeholder: "Your Password"
-                },
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Your Name",
+        },
+        password: {
+          label: "Password",
+          type: "text",
+          placeholder: "Your Password",
+        },
+      },
+      async authorize(credentials): Promise<any> {
+        // get data
+        const users = await allUsers();
 
-            },
-            async authorize(credentials) {
-                // get data
-                const user = {id: "1", name: "bilal", password:"pass"}
+        let foundUser: User = {
+          _id: "",
+          name: "",
+          email: "",
+          isAdmin: "",
+          phone: "",
+          country: "",
+          profilePhoto: "",
+          linkedin: "",
+          additionalInfo: "",
+          password: "",
+        };
 
-                if(credentials?.username === user.name && credentials?.password === user.password) {
-                    return user
-                } else {
-                    return null
-                }
-            }
-        }),
-        
-    ],
-//     pages: {
-//   signIn: '/auth/signin',
-//   signOut: '/auth/signout',
-//   error: '/auth/error', // Error code passed in query string as ?error=
-//   verifyRequest: '/auth/verify-request', // (used for check email message)
-//   newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-// }
-}
+        //
+        users.map((u: User) => {
+          console.log(u);
+
+          if (
+            credentials?.username === u.name &&
+            bcrypt.compareSync(credentials?.password!, u.password)
+          ) {
+            console.log("success");
+            foundUser = u;
+          }
+        });
+        console.log("User: ", foundUser);
+
+        // const user = { id: "1", name: "bilal", password: "pass" };
+
+        if (credentials?.username === foundUser.name) {
+          return foundUser;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  //     pages: {
+  //   signIn: '/auth/signin',
+  //   signOut: '/auth/signout',
+  //   error: '/auth/error', // Error code passed in query string as ?error=
+  //   verifyRequest: '/auth/verify-request', // (used for check email message)
+  //   newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+  // }
+};
+
+const allUsers = async () => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/users`);
+  const users = await response.json();
+
+  return users;
+};
